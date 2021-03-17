e1e773261324a54a0c2b168f5836d70f0ae09ae7
Elena Anghel <80761981+elanghel@users.noreply.github.com>
2021-03-17 15:41:01 +0200

    Task/003 save btn style (#5)
    * 001: disappear keyboard on touching outside the testField and pressing RETURN key
    
    * 002: apply Save Button style : backgroundColor, backgroundColor when highlighted, text color and border radius
    
    * 003: Fixed all issuesI except trying to use UIHoverGestureRecognize
    
    * 004: made class HighlightButton separately

---
+++ GithubPractice/HighlightButton.swift
@@ -0,0 +1,44 @@
+//
+//  HighlightButton.swift
+//  GithubPractice
+//
+//  Created by Elena Anghel on 3/17/21.
+//
+import UIKit
+
+@IBDesignable
+class HighlightButton: UIButton {
+    var highlightDuration: TimeInterval = 0.25
+    
+    @IBInspectable var normalBackgroundColor: UIColor? {
+        didSet {
+            backgroundColor = normalBackgroundColor
+        }
+    }
+    
+    @IBInspectable var highlightedBackgroundColor: UIColor?
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if oldValue == false && isHighlighted {
+                highlight()
+            } else if oldValue == true && !isHighlighted {
+                unHighlight()
+            }
+        }
+    }
+    
+    func highlight() {
+        animateBackground(to: highlightedBackgroundColor, duration: highlightDuration)
+    }
+    
+    func unHighlight() {
+        animateBackground(to: normalBackgroundColor, duration: highlightDuration)
+    }
+    private func animateBackground(to color: UIColor?, duration: TimeInterval) {
+        guard let color = color else { return }
+        UIView.animate(withDuration: highlightDuration) {
+            self.backgroundColor = color
+        }
+    }
+}
--- GithubPractice/ViewController.swift
+++ GithubPractice/ViewController.swift
@@ -7,19 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var firstNameField: UITextField!
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var jobPositionField: UITextField!
     @IBOutlet weak var birthdayLabel: UILabel!
     @IBOutlet weak var birthdayPicker: UIDatePicker!
-    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var saveButton: HighlightButton!
     @IBOutlet weak var clearButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstNameField.delegate = self
+        lastNameField.delegate = self
+        jobPositionField.delegate = self
+        
+        saveButton.layer.cornerRadius = 8
+        
     }
     
     @IBAction func didClickSave(_ sender: Any) {
@@ -29,4 +36,24 @@ class ViewController: UIViewController {
     @IBAction func didClickClear(_ sender: Any) {
         
     }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
+
+
+
