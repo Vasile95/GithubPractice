fb05864c7bf2d7ba938a4bf9a0f24ad184512b80
Elena Anghel <elena.anghel@endava.com>
2021-03-17 14:23:55 +0200

    002: apply Save Button style : backgroundColor, backgroundColor when highlighted, text color and border radius

--- GithubPractice/ViewController.swift
+++ GithubPractice/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var jobPositionField: UITextField!
     @IBOutlet weak var birthdayLabel: UILabel!
     @IBOutlet weak var birthdayPicker: UIDatePicker!
-    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var saveButton: HighlightButton!
     @IBOutlet weak var clearButton: UIButton!
     
     override func viewDidLoad() {
@@ -24,6 +24,8 @@ class ViewController: UIViewController,UITextFieldDelegate {
         firstNameField.delegate = self
         lastNameField.delegate = self
         jobPositionField.delegate = self
+        
+        saveButton.layer.cornerRadius = 8 
     }
     
     @IBAction func didClickSave(_ sender: Any) {
@@ -51,4 +53,43 @@ class ViewController: UIViewController,UITextFieldDelegate {
           textField.resignFirstResponder()
           return true
       }
+    
+    
+}
+
+@IBDesignable
+class HighlightButton: UIButton {
+    var highlightDuration: TimeInterval = 0.25
+    
+    @IBInspectable var normalBackgroundColor: UIColor? {
+        didSet {
+            backgroundColor = normalBackgroundColor
+        }
+    }
+    
+    @IBInspectable var highlightedBackgroundColor: UIColor?
+   
+    override var isHighlighted: Bool {
+        didSet {
+            if oldValue == false && isHighlighted {
+                highlight()
+            } else if oldValue == true && !isHighlighted {
+                unHighlight()
+            }
+        }
+    }
+
+    func highlight() {
+        animateBackground(to: highlightedBackgroundColor, duration: highlightDuration)
+    }
+
+    func unHighlight() {
+        animateBackground(to: normalBackgroundColor, duration: highlightDuration)
+    }
+    private func animateBackground(to color: UIColor?, duration: TimeInterval) {
+        guard let color = color else { return }
+        UIView.animate(withDuration: highlightDuration) {
+            self.backgroundColor = color
+        }
+    }
 }
